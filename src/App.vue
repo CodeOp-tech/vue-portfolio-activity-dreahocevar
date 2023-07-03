e161adf13ab58030ab0f83e11365baca7070c66d
Andrea Hocevar <annhocevar@gmail.com>
2023-07-03 18:02:08 +0200

    fun

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-
-    <button @click="showUserView" :class="{ active: activeView === 'User' }">
-      User
-    </button>
-    <button @click="showAdminView" :class="{ active: activeView === 'Admin' }">
-      Admin
-    </button>
-
-    <component :is="activeView"></component>
-    <admin-view v-if="isAdmin" @createProject="addProject" />
-    <user-view v-else />
+  <div class="container">
+    <h1>hello there!</h1>
+    <div class="button-container">
+      <button
+        :class="{ button: true, 'highligted-button': isAdmin }"
+        @click="($event) => (isAdmin = true)"
+      >
+        admin
+      </button>
+      <button
+        :class="{ button: true, 'highligted-button': !isAdmin }"
+        @click="($event) => (isAdmin = false)"
+      >
+        user
+      </button>
+      <admin-view v-if="isAdmin" @createProject="addProject" />
+      <user-view v-else />
+    </div>
   </div>
 </template>
 
@@ -20,36 +25,93 @@ import AdminView from "./components/AdminView.vue";
 import UserView from "./components/UserView.vue";
 
 export default {
+  name: "app",
   components: {
-    User,
-    Admin,
+    userView: UserView,
+    adminView: AdminView,
   },
   data() {
     return {
-      activeView: "User",
+      isAdmin: true,
+      allProjects: [],
     };
   },
   methods: {
-    showUserView() {
-      this.activeView = "User";
-    },
-    showAdminView() {
-      this.activeView = "Admin";
+    addProject(project) {
+      this.allProjects.push(project);
     },
   },
 };
 </script>
+
 <style>
-button {
-  padding: 10px 20px;
-  background-color: #ccc;
-  color: #fff;
-  border: none;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 }
 
-button.active {
-  background-color: #333;
+.container::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background-color: #5a9367;
+  opacity: 1; /* Establece la opacidad inicial en 1 */
+  animation: fadeCircle 10s linear infinite; /* Agrega una animación de opacidad con duración de 2 segundos y repetición infinita */
+  z-index: -1; /* Coloca el círculo detrás del contenido estableciendo un valor de z-index negativo */
+}
+
+.container:click::before {
+  animation: none; /* Detiene la animación al hacer hover en el contenedor */
+  opacity: 0; /* Establece la opacidad en 0 */
+}
+
+@keyframes fadeCircle {
+  0% {
+    opacity: 0.1; /* Opacidad inicial en 1 */
+  }
+  25% {
+    opacity: 0.5; /* Opacidad en 0 a la mitad de la animación */
+  }
+  50% {
+    opacity: 1; /* Opacidad en 0 a la mitad de la animación */
+  }
+  25% {
+    opacity: 0.5; /* Opacidad en 0 a la mitad de la animación */
+  }
+  100% {
+    opacity: 0.1; /* Opacidad final en 1 */
+  }
+}
+
+.button-container {
+  display: inline-block;
+}
+.button {
+  background-color: rgb(246, 235, 204);
+  font-family: "Josefin Sans";
+  font-size: 18px;
+  border-radius: 5px;
+  margin: 6px;
+  margin-bottom: 15px;
+  width: 80px;
+  height: 28px;
+  border: none;
+}
+
+.highligted-button {
+  background-color: rgb(135, 0, 0);
+  color: rgb(255, 255, 255);
+  border: none;
+}
+h1 {
+  color: white;
 }
 </style>
--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    Title
-    <input v-model="title" />
-    <button @click="handleSubmit">SUBMIT</button>
+  <div class="admin-view">
+    <div class="form-group">
+      <label for="title">URL</label>
+      <input id="title" v-model="title" class="admin-input" />
+    </div>
+    <button @click="handleSubmit" class="admin-button">SUBMIT</button>
   </div>
 </template>
 
@@ -28,4 +30,39 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.admin-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.form-group label {
+  font-family: "Josefin Sans", sans-serif;
+  /* Estilos adicionales para la etiqueta */
+  margin-right: 10px;
+}
+
+.admin-input {
+  font-family: "Josefin Sans", sans-serif;
+  /* Estilos adicionales para el input */
+  width: 300px;
+  height: 30px;
+  font-size: 18px;
+}
+
+.admin-button {
+  font-family: "Josefin Sans", sans-serif;
+  /* Estilos adicionales para el botón */
+  width: 120px;
+  height: 40px;
+  font-size: 18px;
+}
+</style>
